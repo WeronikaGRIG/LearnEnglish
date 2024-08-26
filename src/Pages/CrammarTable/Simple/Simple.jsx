7b63124297cbf86ff8433f165472d5785154c0a6
WeronikaGRIG <veronikaudot@gmail.com>
2024-08-26 16:36:09 +0200

    add styles for table

--- src/Pages/CrammarTable/Simple/Simple.jsx
+++ src/Pages/CrammarTable/Simple/Simple.jsx
@@ -1,5 +1,6 @@
 
 import dataTenses from '../../../Common/dataTensens.json';
+import style from './Simple.module.scss';
 
 export default function Simple() {
     const { simple } = dataTenses;
@@ -11,30 +12,31 @@ export default function Simple() {
     };
 
     return (
-        <div>
-            <h2>Simple Tenses</h2>
-            <p>Простое времена</p>
+        <>
+            <h2 className={style.title}>Simple Tenses</h2>
+            <p className={style.descr}>Простое времена</p>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>Tenses</th>
-                        <th>Вопрос</th>
-                        <th>Утверждение</th>
-                        <th>Отрицание</th>
+            <table className={style.table}>
+                <thead className={style.table__header}>
+                    <tr className={style.table__header_row}>
+                        <th className={style.headerCell_first}>Tenses</th>
+                        <th className={style.headerCell}>Вопрос</th>
+                        <th className={style.headerCell}>Утверждение</th>
+                        <th className={style.headerCell}>Отрицание</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody className={style.table__body}>
                     {Object.keys(simple).map(time => (
-                        <tr key={time}>
-                            <td>{time}</td>
-                            <td>{Array.isArray(simple[time].question) ? renderTextWithStyles(simple[time].question) : simple[time].question}</td>
-                            <td>{Array.isArray(simple[time].affirmative) ? renderTextWithStyles(simple[time].affirmative) : simple[time].affirmative}</td>
-                            <td>{Array.isArray(simple[time].negative) ? renderTextWithStyles(simple[time].negative) : simple[time].negative}</td>
+                        <tr key={time}
+                            className={style.table__body_row}>
+                            <td className={style.cellFirst}>{time}</td>
+                            <td className={style.cell}>{Array.isArray(simple[time].question) ? renderTextWithStyles(simple[time].question) : simple[time].question}</td>
+                            <td className={style.cell}>{Array.isArray(simple[time].affirmative) ? renderTextWithStyles(simple[time].affirmative) : simple[time].affirmative}</td>
+                            <td className={style.cell}>{Array.isArray(simple[time].negative) ? renderTextWithStyles(simple[time].negative) : simple[time].negative}</td>
                         </tr>
                     ))}
                 </tbody>
             </table>
-        </div>
+        </>
     );
 }
